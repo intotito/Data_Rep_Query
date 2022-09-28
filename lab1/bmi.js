57e11a8df3f04c91095097762eb5f2522a63b130
intotito <intotito@gmail.com>
2022-09-29 00:57:49 +0100

    Excercise 1 finished again

--- lab1/bmi.js
+++ lab1/bmi.js
@@ -1,3 +1,11 @@
 class BMI {
-    
+   constructor(height, weight){
+    this.height = height;
+    this.weight = weight;
+   }
+   calculateBMI(){
+    return this.weight / (this.height * this.height);
+   } 
 }
+var bmi = new BMI(1.78, 63);
+console.log(bmi.calculateBMI());
