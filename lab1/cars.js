62ede8a37be49c483a395609c768102a0b8d834e
intotito <intotito@gmail.com>
2022-09-29 01:25:44 +0100

    Excercise 3 finished

---
+++ lab1/cars.js
@@ -0,0 +1,25 @@
+class Vehicle { 
+    constructor(make, model, year) {
+        this.make = make;
+        this.model = model;
+        this.year = year;
+     } 
+
+     information(){
+        console.log(`Make: ${this.make} Model: ${this.model} Year: ${this.year}`);
+     }
+} 
+
+class Car extends Vehicle{
+    constructor(make, model, year, doors){
+        super(make, model, year);
+        this.doors = doors;
+    }
+    information(){
+        super.information();
+        console.log(`Doors: ${this.doors}`);
+    }
+}
+
+var veh = new Car("Mercedez", "AMG 500", 2020, 4);
+veh.information();
