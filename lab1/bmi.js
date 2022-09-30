47385ca1b4594d07e53860b090a8cc93521b1d5f
intotito <intotito@gmail.com>
2022-09-30 11:45:29 +0100

    More comments

--- lab1/bmi.js
+++ lab1/bmi.js
@@ -1,9 +1,20 @@
-// Class declaration
+/**
+ * A class that calculates the Body Mass Index (BMI)
+ */
 class BMI {
+   /**
+    * 
+    * @param {Number} height - Height of the person
+    * @param {Number} weight - Weight of the person
+    */
    constructor(height, weight){
     this.height = height;
     this.weight = weight;
    }
+   /**
+    * 
+    * @returns {Number} - BMI of the person calculated as the ration between the weight and square of the height
+    */
    calculateBMI(){
     return this.weight / (this.height * this.height);
    } 
--- lab1/cars.js
+++ lab1/cars.js
@@ -1,26 +1,48 @@
-// Class declaration
+/**
+ * Base class for Vehicle
+ */
 class Vehicle { 
+    /**
+     * 
+     * @param {String} make - The Make of Vehicle
+     * @param {String} model - The Model of the Vehicle
+     * @param {Number} year - The manufacturing year of the Vehicle
+     */
     constructor(make, model, year) {
         this.make = make;
         this.model = model;
         this.year = year;
      } 
-
+     /**
+      * Displays the Information about the Vehicle
+      */
      information(){
         console.log(`Make: ${this.make} \nModel: ${this.model} \nYear: ${this.year}`);
      }
 } 
 
+/**
+ * A derived class from Vehicle
+ */
 class Car extends Vehicle{
+    /**
+     * 
+     * @param {String} make - The Make of the Car 
+     * @param {String} model - The Model of the Car
+     * @param {Number} year - The manufacturing year of the Car
+     * @param {Number} doors - Number of doorsof the car
+     */
     constructor(make, model, year, doors){
         super(make, model, year);
         this.doors = doors;
     }
+    /**
+     * Overriden method that displays Information about the Car
+     */
     information(){
         super.information();
         console.log(`Doors: ${this.doors}`);
     }
 }
-
 var veh = new Car("Mercedez", "AMG 500", 2020, 4);
 veh.information();
