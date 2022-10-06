cfb3d76dde9bfbd3e7802f6f777de69a7843e475
intotito <intotito@gmail.com>
2022-10-07 00:18:20 +0100

    Lab2 first commit

---
+++ my-app/src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+//import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <h2 className="etiti">Helllo World!</h2>
+    <h2 className="etiti">It is {new Date().toLocaleTimeString()}.</h2>
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
