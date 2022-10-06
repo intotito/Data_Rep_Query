8c5ced75a7c47c0a962608b29eeb957b76e89ced
intotito <intotito@gmail.com>
2022-10-07 00:44:11 +0100

    Header component added

---
+++ my-app/src/components/content.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+export class Header extends React.Component{
+    constructor(props){
+        super(props);
+    }
+    render(){
+        return(
+            <div className="header"></div>
+        );
+    }
+}
